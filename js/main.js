de2b917fec8f9f60637b86dc1e9e44ebf7aaf6f4
okorojames <okorojameschizaram@gmail.com>
2022-12-30 11:34:03 +0100

    ci/cd

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-const billInput = document.querySelector(".bill-input").value.trim();
+const sumBtn = document.querySelector(".sum-btn");
+const fivePer = document.querySelector(".five-per");
+//
+//
+sumBtn.addEventListener("click", displaySum);
+//
+//
+function displaySum() {
+  //
+  // Variables needed.
+  //
+  const priceOne = document.querySelector(".price-one");
+  const priceTwo = document.querySelector(".price-two");
+  let billInput = document.querySelector(".bill-input").value.trim();
+  // billInput.value = Number();
+  let peopleInput = document.querySelector(".people-input").value.trim();
+  // peopleInput.value = Number();
+  //
+  //
+  //
+  const sum = billInput.value + peopleInput.value;
+  console.log(sum);
+  document.querySelector(".bill-input").value = "";
+  document.querySelector(".people-input").value = "";
+}
